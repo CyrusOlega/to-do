78fa46592710ffead10cb824624ce739364d19a3
Cyrus Olega <cyrusabraham.olega.cics@ust.edu.ph>
2024-02-26 13:08:11 +0800

    converted individual sticky note into component

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,18 @@
 export default function Body() {
+  const length = 15;
+  const stickyNotes = [];
+  for (let i = 0; i < length; i++) stickyNotes.push({ id: i });
+
+  return <div id="sticky-notes-container">{stickyNotes.map(StickyNote)}</div>;
+}
+
+function StickyNote() {
   return (
-    <div id="sticky-note-container">
-      <div className="sticky-note">
+    <div className="sticky-note">
+      <div className="title">
         <span>asdasd</span>
       </div>
-      <div className="sticky-note">
+      <div className="content">
         <span>asdasd</span>
       </div>
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
 import { IconContext } from "react-icons";
-import { HiPlus } from "react-icons/hi2";
+import { CiStickyNote } from "react-icons/ci";
 
 export default function Header() {
   return (
     <div id="header">
       <span>What To Do?</span>
       <button>
-        <IconContext.Provider value={{ size: "30px", color: "#ab9649" }}>
-          <HiPlus className="button-icons" />
+        <IconContext.Provider value={{ size: "40px", color: "#766835" }}>
+          <CiStickyNote className="button-icons" />
         </IconContext.Provider>
       </button>
     </div>
