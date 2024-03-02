1ef752391488aa9468ce9a42de391579a7895190
CyrusOlega <cyrusabraham.olega.cics@ust.edu.ph>
2024-03-02 17:49:16 +0800

    added overflow detection

--- src/components/FormModal.js
+++ src/components/FormModal.js
@@ -1,6 +1,18 @@
-import { forwardRef } from "react";
+import { forwardRef, useState } from "react";
+import { IoIosArrowDown } from "react-icons/io";
 
 const FormModal = forwardRef(function FormModal(props, ref) {
+  const [isOverflow, setOverflow] = useState(false);
+
+  const checkOverflow = () => {
+    const textArea = document.getElementById("text-area");
+    if (textArea.clientHeight < textArea.scrollHeight) {
+      setOverflow(true);
+    } else {
+      setOverflow(false);
+    }
+  };
+
   return (
     <div id="form-modal-container">
       <div id="form-modal" ref={ref}>
@@ -9,7 +21,13 @@ const FormModal = forwardRef(function FormModal(props, ref) {
             <input name="Title" placeholder="Title" className="input-form" />
           </div>
           <div id="text-area-container">
-            <textarea name="Content" placeholder="Enter Task..."/>
+            <textarea
+              id="text-area"
+              name="Content"
+              placeholder="Enter Task..."
+              onKeyDown={checkOverflow}
+            />
+            {isOverflow && <IoIosArrowDown />}
           </div>
         </form>
       </div>
